fb0bdfa6ef3a1b9fce0effea482dbc5981dcfc00
Paulo Elias <paulo@pauloelias.com>
2020-09-18 09:43:44 -0700

    Adjust settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,35 @@
 module.exports = {
-  extends: [
-    '@pauloelias/eslint-config-javascript-standard-core',
-    'standard-jsx',
-    'prettier/react',
-    'plugin:jsx-a11y/recommended'
-  ],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true
+    },
     sourceType: 'module'
   },
   env: {
     browser: true,
-    jest: true
+    es6: true,
+    commonjs: true
   },
+  extends: [
+    'standard',
+    'prettier',
+    'standard-jsx',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended'
+  ],
   rules: {
+    'arrow-parens': [2, 'as-needed'],
+    camelcase: 2,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
     'prettier/prettier': [
       'error',
       {
@@ -23,7 +38,19 @@ module.exports = {
         singleQuote: true,
         trailingComma: 'none'
       }
-    ]
+    ],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['invalidHref']
+      }
+    ],
+    'react/prop-types': 0,
+    'react/jsx-uses-vars': [2],
+    'react/jsx-no-undef': 1,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   },
-  plugins: ['jsx-a11y', 'react', 'react-hooks']
+  plugins: ['html', 'prettier', 'standard', 'jsx-a11y', 'react', 'react-hooks']
 }
